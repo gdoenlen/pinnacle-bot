24dd9f144b8e8a51bb356f3b78300fdbfb50f3cb
George Doenlen <gdoenlen@gmail.com>
2023-01-28 17:18:21 -0500

    add db migration generation

---
+++ src/test/java/com/github/gdoenlen/pinnacle/bot/core/migration/GenerateDbMigration.java
@@ -0,0 +1,18 @@
+package com.github.gdoenlen.pinnacle.bot.core.migration;
+
+import java.io.IOException;
+
+import io.ebean.annotation.Platform;
+import io.ebean.dbmigration.DbMigration;
+
+public class GenerateDbMigration {
+    public static void main(String[] args) throws IOException {
+        System.setProperty("ddl.migration.version", "1.0");
+        System.setProperty("ddl.migration.name", "init");
+
+        var migration = DbMigration.create();
+        migration.setPlatform(Platform.H2);
+
+        migration.generateMigration();
+    }
+}

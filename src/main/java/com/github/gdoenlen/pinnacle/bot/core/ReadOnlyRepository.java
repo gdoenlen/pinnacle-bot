1e22c8c3927dd993af6f86670be9d7520bf7de81
George Doenlen <gdoenlen@gmail.com>
2023-04-08 22:20:42 -0400

    mockito latest

---
+++ src/main/java/com/github/gdoenlen/pinnacle/bot/core/ReadOnlyRepository.java
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+package com.github.gdoenlen.pinnacle.bot.core;
+
+import io.ebean.DB;
+import io.ebean.Database;
+import io.ebean.Query;
+
+public abstract class Repository<T> {
+    protected final Class<T> clazz;
+    protected final Database database;
+
+    protected Repository(Class<T> clazz) {
+        this.clazz = clazz;
+        this.database = DB.getDefault();
+    }
+
+    protected Query<T> query() {
+        return this.database.createQuery(this.clazz);
+    }
+}

edc04466dc17b48888d181a73aa9b8ef1e97c441
George Doenlen <gdoenlen@gmail.com>
2023-04-23 09:30:42 -0400

    requireNonNull id

--- src/main/java/com/github/gdoenlen/pinnacle/bot/core/Facade.java
+++ src/main/java/com/github/gdoenlen/pinnacle/bot/core/Facade.java
@@ -1,10 +1,12 @@
 package com.github.gdoenlen.pinnacle.bot.core;
 
+import static java.util.Objects.requireNonNull;
+
 public abstract class Facade<T extends Model> {
 
     protected abstract Repository<T> repository();
 
     public T getReference(Long id) {
-        return this.repository().getReference(id);
+        return this.repository().getReference(requireNonNull(id));
     }
 }

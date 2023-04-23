81ae7d34663d05a41cdfda43aaa515a4c1ba80fd
George Doenlen <gdoenlen@gmail.com>
2023-04-23 09:35:57 -0400

    remove application scoping

--- src/main/java/com/github/gdoenlen/pinnacle/bot/core/ContextualCurrentUserProvider.java
+++ src/main/java/com/github/gdoenlen/pinnacle/bot/core/ContextualCurrentUserProvider.java
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 package com.github.gdoenlen.pinnacle.bot.core;
 
-import jakarta.enterprise.context.ApplicationScoped;
+import com.github.gdoenlen.pinnacle.bot.core.users.User;
 
 import io.ebean.config.CurrentUserProvider;
 
-@ApplicationScoped
 class ContextualCurrentUserProvider implements CurrentUserProvider {
     @Override
-    public Object currentUser() {
-        return Context.get().user().getId();
+    public User currentUser() {
+        return Context.get().user();
     }
 }
